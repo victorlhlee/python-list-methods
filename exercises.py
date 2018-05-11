1731698ce22e777112504aaca8644f7edb22aed2
madycakes <viclee02@gmail.com>
2018-05-10 19:52:39 -1000

    initial commit

--- exercises.py
+++ exercises.py
@@ -5,6 +5,7 @@
 
 hacker_legends = ['LulzSec', 'Gary McKinnon', 'Adrian Lamo', 'Jonathan James', 'Kevin Poulsen']
 
+print(hacker_legends)
 #insert (index, element) adds a new element at any position in your list.
 #2. You just created a networking study list and forget to add in 'SSH'. Please add that into the 3rd position in the networking list and print your results.
 
